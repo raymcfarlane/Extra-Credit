50e4fbc5e1c41ad9f20fb46734f918a5fb13e269
raymcfarlane <122712555+raymcfarlane@users.noreply.github.com>
2023-07-09 15:18:54 -0400

    extra credit work

---
+++ README.md
@@ -0,0 +1,38 @@
+# Extra Credit- Implentation of a Reducer
+ 
+## Table of Contents:
+  - [Description](#description)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+ 
+## Description
+```md
+I added comments describing the functionality of the code found in [reducers.js]
+
+What is the purpose of the action argument that gets passed to the reducer function?
+
+How can we account for multiple types of actions inside the reducer?
+
+Why do we create variable names for each action type in `actions.js`?
+
+How do we make sure that the database is seeded?
+
+Which `npm` package allows us to run both the front end and the back end at the same time? How do you start it?
+
+
+```
+
+## Test
+no
+
+## License
+Copyright <2023> <Raynica McFarlane>
+
+## Questions
+
+Have additional questions? Click the links below to reach me through my GitHub account or Email address.
+
+[Link to Github](https://github.com/raymcfarlane)
+
+<a href="mailto:raynicamcfarlane@gmail.com">raynicamcfarlane@gmail.com</a>
---
+++ Solved/client/src/utils/reducers.js
@@ -0,0 +1,93 @@
+import { useReducer } from 'react';
+import {
+  UPDATE_PRODUCTS,
+  ADD_TO_CART,
+  UPDATE_CART_QUANTITY,
+  REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  CLEAR_CART,
+  TOGGLE_CART,
+} from './actions';
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    // Update products in cart, add a new item to cart, add multiple items to cart 
+    case UPDATE_PRODUCTS:
+      return {
+        ...state,
+        products: [...action.products],
+      };
+
+    case ADD_TO_CART:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+
+    case ADD_MULTIPLE_TO_CART:
+      return {
+        ...state,
+        cart: [...state.cart, ...action.products],
+      };
+    // update the item's that are in the cart quantity 
+    case UPDATE_CART_QUANTITY:
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+
+    // remove an item from the cart, clearing cart out completelu, update selected categories 
+    case REMOVE_FROM_CART:
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
+
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+
+    case CLEAR_CART:
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+
+    case TOGGLE_CART:
+      return {
+        ...state,
+        cartOpen: !state.cartOpen,
+      };
+
+    case UPDATE_CATEGORIES:
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+
+    case UPDATE_CURRENT_CATEGORY:
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+
+    // when no changes were made, cart returns to default 
+    default:
+      return state;
+  }
+};
+
+export function useProductReducer(initialState) {
+  return useReducer(reducer, initialState);
+}
